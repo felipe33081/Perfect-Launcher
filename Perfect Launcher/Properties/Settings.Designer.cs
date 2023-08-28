d413dd9e960b8ab0c0d2e82a930b26d433e89b1e
felipe33081 <felipenogueirap7@gmail.com>
2023-08-28 16:16:02 -0300

    adjust

--- Perfect Launcher/Properties/Settings.Designer.cs
+++ Perfect Launcher/Properties/Settings.Designer.cs
@@ -25,65 +25,6 @@ namespace Perfect_Launcher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring />\r\n</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection User {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["User"]));
-            }
-            set {
-                this["User"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring />\r\n</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection Passwd {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["Passwd"]));
-            }
-            set {
-                this["Passwd"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring />\r\n</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection Nick
-        {
-            get
-            {
-                return ((global::System.Collections.Specialized.StringCollection)(this["Nick"]));
-            }
-            set
-            {
-                this["Nick"] = value;
-            }
-        }
-
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring />\r\n</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection Classe {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["Classe"]));
-            }
-            set {
-                this["Classe"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool bFirstRun {
             get {
@@ -91,20 +32,6 @@ namespace Perfect_Launcher.Properties {
             }
             set {
                 this["bFirstRun"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\r\n  <s" +
-            "tring />\r\n</ArrayOfString>")]
-        public global::System.Collections.Specialized.StringCollection ComboQueue {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["ComboQueue"]));
-            }
-            set {
-                this["ComboQueue"] = value;
             }
         }
         
@@ -218,31 +145,6 @@ namespace Perfect_Launcher.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
-<ArrayOfBoolean xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-  <boolean>false</boolean>
-</ArrayOfBoolean>")]
-        public bool[] DailyCheckups {
-            get {
-                return ((bool[])(this["DailyCheckups"]));
-            }
-            set {
-                this["DailyCheckups"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
         public bool bCheckedForUpdates {
             get {
@@ -262,19 +164,6 @@ namespace Perfect_Launcher.Properties {
             }
             set {
                 this["CrashWatchInterval"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsi=\"http://www.w3." +
-            "org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" />")]
-        public global::System.Collections.Specialized.StringCollection UsersBeforeClosing {
-            get {
-                return ((global::System.Collections.Specialized.StringCollection)(this["UsersBeforeClosing"]));
-            }
-            set {
-                this["UsersBeforeClosing"] = value;
             }
         }
         
@@ -347,6 +236,114 @@ namespace Perfect_Launcher.Properties {
             }
             set {
                 this["ContaAnteriorBtn"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring />\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection User {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["User"]));
+            }
+            set {
+                this["User"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring />\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection Passwd {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["Passwd"]));
+            }
+            set {
+                this["Passwd"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring />\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection Classe {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["Classe"]));
+            }
+            set {
+                this["Classe"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring />\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection Nick {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["Nick"]));
+            }
+            set {
+                this["Nick"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
+            "tring />\r\n</ArrayOfString>")]
+        public global::System.Collections.Specialized.StringCollection ComboQueue {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["ComboQueue"]));
+            }
+            set {
+                this["ComboQueue"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfBoolean xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+  <boolean>false</boolean>
+</ArrayOfBoolean>")]
+        public bool[] DailyCheckups {
+            get {
+                return ((bool[])(this["DailyCheckups"]));
+            }
+            set {
+                this["DailyCheckups"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
+            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" />")]
+        public global::System.Collections.Specialized.StringCollection UsersBeforeClosing {
+            get {
+                return ((global::System.Collections.Specialized.StringCollection)(this["UsersBeforeClosing"]));
+            }
+            set {
+                this["UsersBeforeClosing"] = value;
             }
         }
     }
